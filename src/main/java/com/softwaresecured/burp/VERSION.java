2815999b0303a655ca80ef3acff4a58c04e943e4
fi7z <dev@softwaresecured.com>
2025-12-22 20:27:39 -0500

    1.0.0

--- src/main/java/com/softwaresecured/burp/VERSION.java
+++ src/main/java/com/softwaresecured/burp/VERSION.java
@@ -1,9 +1,9 @@
 package com.softwaresecured.burp;
 
 public final class VERSION {
-    public static final int VERSION_MAJOR = 0;
+    public static final int VERSION_MAJOR = 1;
     public static final int VERSION_MINOR = 0;
-    public static final int VERSION_PATCH = 1;
+    public static final int VERSION_PATCH = 0;
     public static String getVersionStrPlain() {
         return String.format("%d.%d.%d", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
     }
